72f0a2b0b2cc217bba58901612c71800a36a34bd
Jean-Baptiste Kaiser <jb@marmelab.com>
2022-02-07 18:37:01 +0100

    Add first test on configfile01.json

--- src/index.spec.ts
+++ src/index.spec.ts
@@ -1,7 +1,16 @@
 import expect from "expect";
+import { CellValue, GameState } from "./gamestate";
+import { loadGameStateFromFile } from "./parseConfigFile";
 
-describe("initial test", () => {
-  it("should pass", () => {
-    expect(true).toBe(true);
+describe("load game state from a valid config file", () => {
+  it("should load configfile01.json without error", () => {
+    const expected: GameState = {
+      board: [
+        [ { value: CellValue.Black }, { value: CellValue.Empty }, { value: CellValue.Black } ],
+        [ { value: CellValue.Empty }, { value: CellValue.Empty }, { value: CellValue.Empty } ],
+        [ { value: CellValue.Empty }, { value: CellValue.Empty }, { value: CellValue.Empty } ]
+      ]
+    };
+    expect(loadGameStateFromFile("src/fixtures/configfile01.json")).toStrictEqual(expected);
   });
 });
