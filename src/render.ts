b145b7bea8e7ebb9281a0008af1330f1f2a69c9d
atilbian <arnaud@marmelab.com>
2022-02-08 10:38:25 +0100

    Extract consts from function

--- src/render.ts
+++ src/render.ts
@@ -1,20 +1,23 @@
 import { GameState, CellValue } from "./gamestate";
 
+const spaceCharacter = " ";
+const lineSeparatorCharacter = "\n";
+const renderedBlackPawn = "⬢";
+const renderedNoPawn = "⬡";
+
 export function displayGameState(gameState: GameState) {
   const renderedGameState = getRenderedGameState(gameState);
   console.log(renderedGameState);
 }
 
 export function getRenderedGameState(gameState: GameState): string {
-  const spaceCharacter = " ";
-  const lineSeparatorCharacter = "\n";
-
   let result = "";
   gameState.board.forEach((row, index) => {
     const initialSpace = spaceCharacter.repeat(index);
     let renderedRow = initialSpace;
     row.forEach((cell) => {
-      const renderedCell = cell.value == CellValue.Black ? "⬢" : "⬡";
+      const renderedCell =
+        cell.value == CellValue.Black ? renderedBlackPawn : renderedNoPawn;
       renderedRow += renderedCell;
     });
     result += renderedRow;
