7d63b80f24e728191a470fc872c4c646a50dbdac
Jean-Baptiste Kaiser <jb@marmelab.com>
2022-02-09 16:11:03 +0100

    use multiline string representation to improve tests readability

--- src/gamestate.spec.ts
+++ src/gamestate.spec.ts
@@ -1,16 +1,30 @@
 import expect from "expect";
 import { GameState, playerHasWon } from "./gamestate";
 
+function parseGameStateFromMultilineString(gameStateStr: string): GameState {
+    return {
+        board: gameStateStr.split(/\r?\n/)
+            .filter(line => line.replace(/ /g, '').length > 0)
+            .map(line => line.replace(/ /g, '').split('')
+                .map(cell => {
+                    switch (cell) {
+                        case "⬢":
+                            return { value: "black" };
+                        default:
+                            return { value: "empty" };
+                    }
+                }))
+    };
+}
+
 describe("check whether player has won the game", () => {
     it("should say WIN with a 3x3 board with a vertical path", () => {
         // given
-        const input: GameState = {
-            board: [
-                [{ value: "empty" }, { value: "black" }, { value: "empty" }],
-                [{ value: "empty" }, { value: "black" }, { value: "empty" }],
-                [{ value: "empty" }, { value: "black" }, { value: "empty" }],
-            ],
-        };
+        const input = parseGameStateFromMultilineString(`
+⬡ ⬢ ⬡
+ ⬡ ⬢ ⬡
+  ⬡ ⬢ ⬡
+        `);
         // when
         const ouput = playerHasWon(input);
         // then
@@ -19,13 +33,11 @@ describe("check whether player has won the game", () => {
 
     it("should say WIN with a 3x3 board with an horizontal path", () => {
         // given
-        const input: GameState = {
-            board: [
-                [{ value: "empty" }, { value: "empty" }, { value: "empty" }],
-                [{ value: "black" }, { value: "black" }, { value: "black" }],
-                [{ value: "empty" }, { value: "empty" }, { value: "empty" }],
-            ],
-        };
+        const input = parseGameStateFromMultilineString(`
+⬡ ⬡ ⬡
+ ⬢ ⬢ ⬢
+  ⬡ ⬡ ⬡
+        `);
         // when
         const ouput = playerHasWon(input);
         // then
@@ -34,13 +46,11 @@ describe("check whether player has won the game", () => {
 
     it("should NOT say WIN with a 3x3 board with a first diagonal", () => {
         // given
-        const input: GameState = {
-            board: [
-                [{ value: "black" }, { value: "empty" }, { value: "empty" }],
-                [{ value: "empty" }, { value: "black" }, { value: "empty" }],
-                [{ value: "empty" }, { value: "empty" }, { value: "black" }],
-            ],
-        };
+        const input = parseGameStateFromMultilineString(`
+⬢ ⬡ ⬡
+ ⬡ ⬢ ⬡
+  ⬡ ⬡ ⬢
+        `);
         // when
         const ouput = playerHasWon(input);
         // then
@@ -49,13 +59,11 @@ describe("check whether player has won the game", () => {
 
     it("should say WIN with a 3x3 board with an anti diagonal", () => {
         // given
-        const input: GameState = {
-            board: [
-                [{ value: "empty" }, { value: "empty" }, { value: "black" }],
-                [{ value: "empty" }, { value: "black" }, { value: "empty" }],
-                [{ value: "black" }, { value: "empty" }, { value: "empty" }],
-            ],
-        };
+        const input = parseGameStateFromMultilineString(`
+⬡ ⬡ ⬢
+ ⬡ ⬢ ⬡
+  ⬢ ⬡ ⬡
+        `);
         // when
         const ouput = playerHasWon(input);
         // then
@@ -64,13 +72,11 @@ describe("check whether player has won the game", () => {
 
     it("should NOT say WIN with a 3x3 board with an incomplete vertical path", () => {
         // given
-        const input: GameState = {
-            board: [
-                [{ value: "empty" }, { value: "black" }, { value: "empty" }],
-                [{ value: "empty" }, { value: "black" }, { value: "empty" }],
-                [{ value: "empty" }, { value: "empty" }, { value: "empty" }],
-            ],
-        };
+        const input = parseGameStateFromMultilineString(`
+⬡ ⬢ ⬡
+ ⬡ ⬢ ⬡
+  ⬡ ⬡ ⬡
+        `);
         // when
         const ouput = playerHasWon(input);
         // then
@@ -79,15 +85,13 @@ describe("check whether player has won the game", () => {
 
     it("should say WIN with this 5x5 board with a continuous line", () => {
         // given
-        const input: GameState = {
-            board: [
-                [{ value: "empty" }, { value: "black" }, { value: "empty" }, { value: "empty" }, { value: "empty" }],
-                [{ value: "empty" }, { value: "black" }, { value: "black" }, { value: "black" }, { value: "empty" }],
-                [{ value: "empty" }, { value: "empty" }, { value: "empty" }, { value: "black" }, { value: "black" }],
-                [{ value: "empty" }, { value: "empty" }, { value: "empty" }, { value: "black" }, { value: "empty" }],
-                [{ value: "empty" }, { value: "empty" }, { value: "black" }, { value: "empty" }, { value: "empty" }],
-            ],
-        };
+        const input = parseGameStateFromMultilineString(`
+⬡ ⬢ ⬡ ⬡ ⬡
+ ⬡ ⬢ ⬢ ⬢ ⬡
+  ⬡ ⬡ ⬡ ⬢ ⬢
+   ⬡ ⬡ ⬡ ⬢ ⬡
+    ⬡ ⬡ ⬢ ⬡ ⬡
+        `);
         // when
         const ouput = playerHasWon(input);
         // then
@@ -96,15 +100,13 @@ describe("check whether player has won the game", () => {
 
     it("should NOT say WIN with this 5x5 board with a non continuous line", () => {
         // given
-        const input: GameState = {
-            board: [
-                [{ value: "black" }, { value: "empty" }, { value: "empty" }, { value: "empty" }, { value: "empty" }],
-                [{ value: "empty" }, { value: "black" }, { value: "black" }, { value: "black" }, { value: "empty" }],
-                [{ value: "empty" }, { value: "empty" }, { value: "empty" }, { value: "black" }, { value: "black" }],
-                [{ value: "empty" }, { value: "empty" }, { value: "empty" }, { value: "black" }, { value: "empty" }],
-                [{ value: "empty" }, { value: "empty" }, { value: "black" }, { value: "empty" }, { value: "empty" }],
-            ],
-        };
+        const input = parseGameStateFromMultilineString(`
+⬢ ⬡ ⬡ ⬡ ⬡
+ ⬡ ⬢ ⬢ ⬢ ⬡
+  ⬡ ⬡ ⬡ ⬢ ⬢
+   ⬡ ⬡ ⬡ ⬢ ⬡
+    ⬡ ⬡ ⬢ ⬡ ⬡
+        `);
         // when
         const ouput = playerHasWon(input);
         // then
@@ -113,15 +115,13 @@ describe("check whether player has won the game", () => {
 
     it("should say WIN with this 5x5 board with a Z", () => {
         // given
-        const input: GameState = {
-            board: [
-                [{ value: "empty" }, { value: "empty" }, { value: "empty" }, { value: "empty" }, { value: "empty" }],
-                [{ value: "black" }, { value: "black" }, { value: "black" }, { value: "black" }, { value: "empty" }],
-                [{ value: "empty" }, { value: "empty" }, { value: "black" }, { value: "empty" }, { value: "empty" }],
-                [{ value: "empty" }, { value: "black" }, { value: "black" }, { value: "black" }, { value: "black" }],
-                [{ value: "empty" }, { value: "empty" }, { value: "empty" }, { value: "empty" }, { value: "empty" }],
-            ],
-        };
+        const input = parseGameStateFromMultilineString(`
+⬡ ⬡ ⬡ ⬡ ⬡
+ ⬢ ⬢ ⬢ ⬢ ⬡
+  ⬡ ⬡ ⬢ ⬡ ⬡
+   ⬡ ⬢ ⬢ ⬢ ⬢
+    ⬡ ⬡ ⬡ ⬡ ⬡
+        `);
         // when
         const ouput = playerHasWon(input);
         // then
