ea6f067ef7439225a41ca336c20033ad57ead513
atilbian <arnaud@marmelab.com>
2022-02-09 16:12:24 +0100

    Use regex in mapping of user/board coords

--- src/user.ts
+++ src/user.ts
@@ -3,24 +3,17 @@ import { GameState } from "./gamestate";
 import { UTF16_CODE_OF_LETTER_A, Coordinates } from "./utils";
 
 export function getBoardCoordinatesFromUserInput(
-  userCoordinates: string
+  userInput: string
 ): Coordinates {
-  let x: number, y: number;
-  if (userCoordinates.length < 2) {
+  const regexValidationUserInput = /^(A)([0-9]*)$/;
+  if (!regexValidationUserInput.test(userInput)) {
     throw new Error(
-      `Given coordinates is not compatible. Must be something like "H4"`
+      `Given coordinates aren't valid. Must be something like "H4"`
     );
-  }
-  if (/[A-Z]/.test(userCoordinates[0])) {
-    x = userCoordinates[0].charCodeAt(0) - UTF16_CODE_OF_LETTER_A;
   } else {
-    throw new Error(`Given character is not included in the range [A,Z]`);
-  }
-  y = parseInt(userCoordinates.slice(1, userCoordinates.length));
-  if (y >= 0 && y <= 25) {
-    return { x: x, y: y };
-  } else {
-    throw new Error(`Given number is not included in the range of [0,25]`);
+    const x = userInput[0].charCodeAt(0) - UTF16_CODE_OF_LETTER_A;
+    const y = parseInt(userInput.slice(1, userInput.length));
+    return { x, y };
   }
 }
 
@@ -28,16 +21,10 @@ export function areCoordinatesValid(
   gameState: GameState,
   coordinates: Coordinates
 ): boolean {
-  if (coordinates.y < 0 || coordinates.y >= gameState.board.length) {
-    return false;
-  }
-  if (
-    coordinates.x < 0 ||
-    coordinates.x >= gameState.board[coordinates.y].length
-  ) {
-    return false;
-  }
-  return true;
+  return (
+    coordinates.y < gameState.board.length &&
+    coordinates.x < gameState.board[coordinates.y].length
+  );
 }
 
 export function askCellCoordinatesToUser(): Promise<string> {
