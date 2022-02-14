08d5c099a9779f615f8d927616ea71aafa121307
atilbian <arnaud@marmelab.com>
2022-02-14 14:56:47 +0100

    Add a template to render the board state and use it

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,13 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Render } from '@nestjs/common';
 import { AppService } from './app.service';
 import { GameState } from './common/gameState';
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) { }
+  constructor(private readonly appService: AppService) {}
 
   @Get()
+  @Render('index')
   getBoardStateFromFile(): { gameState: GameState } {
     return this.appService.getBoardStateFromFile();
   }
