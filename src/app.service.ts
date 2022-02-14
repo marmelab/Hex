0523bf76b6563d1cd7cbc56a435c477ec2ab8ed6
atilbian <arnaud@marmelab.com>
2022-02-14 14:55:19 +0100

    Make service load game state from file

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,8 +1,16 @@
 import { Injectable } from '@nestjs/common';
+import { parseGameStateFromFile } from './common/parseConfigFile';
+import { join } from 'path';
+import { GameState } from './common/gameState';
+
+const configPathFromDistDir = '../gameStateFile.json';
 
 @Injectable()
 export class AppService {
-  getHello(): string {
-    return 'Hello World!';
+  getBoardStateFromFile(): { gameState: GameState } {
+    const gameState = parseGameStateFromFile(
+      join(__dirname, configPathFromDistDir),
+    );
+    return { gameState };
   }
 }
