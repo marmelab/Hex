9f236fd8f859aebb83a985b9545db0ddde56e0ee
Jean-Baptiste Kaiser <jb@marmelab.com>
2022-02-15 13:34:09 +0100

    Improve board representation using CSS

--- src/main.ts
+++ src/main.ts
@@ -2,13 +2,24 @@ import { NestFactory } from '@nestjs/core';
 import { NestExpressApplication } from '@nestjs/platform-express';
 import { AppModule } from './app.module';
 import { join } from 'path';
+import * as hbs from 'hbs';
 
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
   app.useStaticAssets(join(__dirname, '..', 'public'));
   app.setBaseViewsDir(join(__dirname, '..', 'views'));
+  registerHandlebarsHelpers();
   app.setViewEngine('hbs');
   await app.listen(3000);
+}
+
+function registerHandlebarsHelpers() {
+  hbs.handlebars.registerHelper('ifCond', function (v1, v2, options) {
+    if (v1 === v2) {
+      return options.fn(this);
+    }
+    return options.inverse(this);
+  });
 }
 
 bootstrap();
