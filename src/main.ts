50e63c21bf89d1bfd7d210b7d7d839b333602a79
atilbian <arnaud@marmelab.com>
2022-02-16 10:25:16 +0100

    Remove hbs helper and set cell color based on attr. Fix #19

--- src/main.ts
+++ src/main.ts
@@ -2,13 +2,11 @@ import { NestFactory } from '@nestjs/core';
 import { NestExpressApplication } from '@nestjs/platform-express';
 import { AppModule } from './app.module';
 import { join } from 'path';
-import { registerHandlebarsHelpers } from './handlebars/helpers';
 
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
   app.useStaticAssets(join(__dirname, '..', 'public'));
   app.setBaseViewsDir(join(__dirname, '..', 'views'));
-  registerHandlebarsHelpers();
   app.setViewEngine('hbs');
   const port = process.env.PORT || 3000;
   await app.listen(port);
--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -3,7 +3,6 @@ import { NestExpressApplication } from '@nestjs/platform-express';
 import request from 'supertest';
 import { AppModule } from './../src/app.module';
 import { join } from 'path';
-import { registerHandlebarsHelpers, unregisterHandlebarsHelpers } from '../src/handlebars/helpers';
 
 describe('AppController (e2e)', () => {
   let app: NestExpressApplication;
@@ -16,13 +15,11 @@ describe('AppController (e2e)', () => {
     app = moduleFixture.createNestApplication();
     app.useStaticAssets(join(__dirname, '..', 'public'));
     app.setBaseViewsDir(join(__dirname, '..', 'views'));
-    registerHandlebarsHelpers();
     app.setViewEngine('hbs');
     await app.init();
   });
 
   afterAll(async () => {
-    unregisterHandlebarsHelpers();
     await app.close();
   });
 
@@ -30,8 +27,8 @@ describe('AppController (e2e)', () => {
     return request(app.getHttpServer())
       .get('/')
       .expect(200)
-      .expect(/<div style="background: #eea">/)
-      .expect(/<div style="background: black">/)
-      .expect(/<div style="background: white">/);
+      .expect(/<div class="cell" data-color="empty">/)
+      .expect(/<div class="cell" data-color="black">/)
+      .expect(/<div class="cell" data-color="white">/);
   });
 });
