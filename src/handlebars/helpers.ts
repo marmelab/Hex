acb4fd7568539bc35a1a7f556da4aa343fe994dd
atilbian <arnaud@marmelab.com>
2022-02-16 12:50:57 +0100

    Create helper to encode gamestate in forms

--- src/handlebars/helpers.ts
+++ src/handlebars/helpers.ts
@@ -2,7 +2,14 @@ import { handlebars } from 'hbs';
 import { stringify } from 'qs';
 
 export function registerHandlebarsHelpers() {
+  handlebars.registerHelper(
+    'encodeObjectAsQueryString',
+    function (objectToEncode: Object): string {
+      return stringify(objectToEncode);
+    },
+  );
 }
 
 export function unregisterHandlebarsHelpers() {
+  handlebars.unregisterHelper('encodeObjectAsQueryString');
 }
