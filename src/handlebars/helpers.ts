c42475bbd6bc44b21d9e76273d5285aa6c687ef2
atilbian <arnaud@marmelab.com>
2022-02-18 11:07:24 +0100

    Add hbs helper to check if cell is in winning path

--- src/handlebars/helpers.ts
+++ src/handlebars/helpers.ts
@@ -1,5 +1,5 @@
 import { handlebars } from 'hbs';
-import { encodeObjectForQueryString } from '../common/utils';
+import { encodeObjectForQueryString, Coordinates } from '../common/utils';
 
 export function registerHandlebarsHelpers() {
   handlebars.registerHelper(
@@ -14,9 +14,25 @@ export function registerHandlebarsHelpers() {
     }
     return options.inverse(this);
   });
+  handlebars.registerHelper(
+    'isCellInWinningPath',
+    function (
+      winningPath: Coordinates[],
+      cellXCoord: number,
+      cellYCoord: number,
+    ) {
+      return (
+        winningPath &&
+        winningPath.filter(
+          (cell) => cell.x == cellXCoord && cell.y == cellYCoord,
+        ).length == 1
+      );
+    },
+  );
 }
 
 export function unregisterHandlebarsHelpers() {
   handlebars.unregisterHelper('encodeObjectAsQueryString');
   handlebars.unregisterHelper('areEqual');
+  handlebars.unregisterHelper('isCellInWinningPath');
 }
