62c7e28ee9d1e62a010eb1c045e9317a553d4366
atilbian <arnaud@marmelab.com>
2022-02-21 18:13:30 +0100

    Display a simple hexa

--- mobile-app/App.tsx
+++ mobile-app/App.tsx
@@ -1,11 +1,16 @@
+import * as React from 'react';
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import Svg, { Polygon } from 'react-native-svg';
 
 export default function App() {
   return (
     <View style={styles.container}>
       <Text>Hex game for mobile</Text>
       <StatusBar style="auto" />
+      <Svg width="100%" height="300">
+        <Polygon points="300,150 225,280 75,280 0,150 75,20 225,20" stroke="blue" strokeWidth="2.5" fill="green" />
+      </Svg>
     </View>
   );
 }
