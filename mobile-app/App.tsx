6410b6c1066e9ad60c5450ceebc0c33e9f416283
atilbian <arnaud@marmelab.com>
2022-02-21 19:50:17 +0100

    Add some const and clean calcs

--- mobile-app/App.tsx
+++ mobile-app/App.tsx
@@ -12,38 +12,45 @@ export default function App() {
   );
 }
 
+
+const DEFAULT_HEXA_COLOR = "black";
+const PLAYER_1_HEXA_COLOR = "blue";
+const PLAYER_2_HEXA_COLOR = "red";
+const HEXA_STROKE_COLOR = "white";
+const DEFAULT_BOARD_SIZE = 9;
+const HEXA_SIZE = 20;
+
 const BoardGame = () => {
   return (
-    <Svg width="100%" height="500">
-      {generateBoard(9, 20).map((hexaToDraw) => (
-        <Polygon points={hexaToDraw.points} stroke="white" fill={hexaToDraw.color} />
+    <Svg width="100%" height="100%">
+      {generateBoard(DEFAULT_BOARD_SIZE).map((hexaToDraw) => (
+        <Polygon points={hexaToDraw.points} stroke={HEXA_STROKE_COLOR} fill={hexaToDraw.color} />
       ))}
     </Svg>
   );
 }
 
-function generateBoard(size: number, hexaSize: number): { points: string, color: string }[] {
+function generateBoard(size: number): { points: string, color: string }[] {
   const sizeWithBorder = size + 2;
   const hexaToDraw = [];
+  const offset = (Math.sqrt(3) * HEXA_SIZE) / 2;
   for (let col = 0; col < sizeWithBorder; col += 1) {
     for (let row = 0; row < sizeWithBorder; row += 1) {
       if (!isHexaFromTopLeftOrBottomRight(col, row, sizeWithBorder)) {
-        let offset = (Math.sqrt(3) * hexaSize) / 2;
-        let x = 40 + offset * col * 2;
-        let y = 40 + offset * row * Math.sqrt(3);
-        x += (offset * (row + 1)); // move each row a little more to the right
-        hexaToDraw.push({ points: getSvgPoints(x, y, hexaSize), color: getColor(col, row, sizeWithBorder) });
+        const x = offset * (1 + col) * 2 + offset * row;
+        const y = offset * (1 + row) * Math.sqrt(3);
+        hexaToDraw.push({ points: getSvgPoints(x, y), color: getColor(col, row, sizeWithBorder) });
       }
     }
   }
   return hexaToDraw;
 }
 
-function getSvgPoints(x: any, y: any, hexaSize: number): string {
+function getSvgPoints(x: any, y: any): string {
   const points = [];
   for (let theta = 0; theta < Math.PI * 2; theta += Math.PI / 3) {
-    let pointX = x + hexaSize * Math.sin(theta);
-    let pointY = y + hexaSize * Math.cos(theta);
+    let pointX = x + HEXA_SIZE * Math.sin(theta);
+    let pointY = y + HEXA_SIZE * Math.cos(theta);
     points.push(pointX + ',' + pointY);
   }
   return points.join(' ');
@@ -52,10 +59,6 @@ function getSvgPoints(x: any, y: any, hexaSize: number): string {
 function isHexaFromTopLeftOrBottomRight(col: number, row: number, boardSizeWithBorder: number) {
   return (col == 0 && row == 0 || col == boardSizeWithBorder - 1 && row == boardSizeWithBorder - 1);
 }
-
-const DEFAULT_HEXA_COLOR = "black";
-const PLAYER_1_HEXA_COLOR = "blue";
-const PLAYER_2_HEXA_COLOR = "red";
 
 function getColor(col: number, row: number, boardSizeWithBorder: number) {
   if (col == 0 && row != boardSizeWithBorder - 1 || col == boardSizeWithBorder - 1 && row != 0) {
