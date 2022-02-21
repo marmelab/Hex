f35b70a03d32d660db988a542bce4db3e657dd99
atilbian <arnaud@marmelab.com>
2022-02-21 19:31:52 +0100

    Draw an empty board

--- mobile-app/App.tsx
+++ mobile-app/App.tsx
@@ -1,24 +1,76 @@
 import * as React from 'react';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, View } from 'react-native';
 import Svg, { Polygon } from 'react-native-svg';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Hex game for mobile</Text>
       <StatusBar style="auto" />
-      <Svg width="100%" height="300">
-        <Polygon points="300,150 225,280 75,280 0,150 75,20 225,20" stroke="blue" strokeWidth="2.5" fill="green" />
-      </Svg>
+      <BoardGame />
     </View>
   );
+}
+
+const BoardGame = () => {
+  return (
+    <Svg width="100%" height="500">
+      {generateBoard(9, 20).map((hexaToDraw) => (
+        <Polygon points={hexaToDraw.points} stroke="white" fill={hexaToDraw.color} />
+      ))}
+    </Svg>
+  );
+}
+
+function generateBoard(size: number, hexaSize: number): { points: string, color: string }[] {
+  const sizeWithBorder = size + 2;
+  const hexaToDraw = [];
+  for (let col = 0; col < sizeWithBorder; col += 1) {
+    for (let row = 0; row < sizeWithBorder; row += 1) {
+      if (!isHexaFromTopLeftOrBottomRight(col, row, sizeWithBorder)) {
+        let offset = (Math.sqrt(3) * hexaSize) / 2;
+        let x = 40 + offset * col * 2;
+        let y = 40 + offset * row * Math.sqrt(3);
+        x += (offset * (row + 1)); // move each row a little more to the right
+        hexaToDraw.push({ points: getSvgPoints(x, y, hexaSize), color: getColor(col, row, sizeWithBorder) });
+      }
+    }
+  }
+  return hexaToDraw;
+}
+
+function getSvgPoints(x: any, y: any, hexaSize: number): string {
+  const points = [];
+  for (let theta = 0; theta < Math.PI * 2; theta += Math.PI / 3) {
+    let pointX = x + hexaSize * Math.sin(theta);
+    let pointY = y + hexaSize * Math.cos(theta);
+    points.push(pointX + ',' + pointY);
+  }
+  return points.join(' ');
+}
+
+function isHexaFromTopLeftOrBottomRight(col: number, row: number, boardSizeWithBorder: number) {
+  return (col == 0 && row == 0 || col == boardSizeWithBorder - 1 && row == boardSizeWithBorder - 1);
+}
+
+const DEFAULT_HEXA_COLOR = "black";
+const PLAYER_1_HEXA_COLOR = "blue";
+const PLAYER_2_HEXA_COLOR = "red";
+
+function getColor(col: number, row: number, boardSizeWithBorder: number) {
+  if (col == 0 && row != boardSizeWithBorder - 1 || col == boardSizeWithBorder - 1 && row != 0) {
+    return PLAYER_1_HEXA_COLOR;
+  }
+  if (row == 0 || row == boardSizeWithBorder - 1) {
+    return PLAYER_2_HEXA_COLOR;
+  }
+  return DEFAULT_HEXA_COLOR;
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: 'black',
     alignItems: 'center',
     justifyContent: 'center',
   },
