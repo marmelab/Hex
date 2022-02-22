493c84508c78a9d88807ba2aad584565e3285b50
atilbian <arnaud@marmelab.com>
2022-02-22 18:50:16 +0100

    Add tests

--- mobile-app/components/Board/BoardService.test.ts
+++ mobile-app/components/Board/BoardService.test.ts
@@ -24,4 +24,86 @@ describe('Generate board cells', () => {
 
     expect(nbOfGeneratedCells).toBe(nbOfCellsInGameState + nbOfBorderCellsAdded);
   });
+  it("Should returns valid data for first border cell", () => {
+    const gameState: GameState = {
+      turn: 'white',
+      winner: null,
+      winningPath: null,
+      board: [
+        [{ value: 'black' }, { value: 'empty' }, { value: 'empty' }, { value: 'empty' }, { value: 'empty' }, { value: 'empty' }],
+        [{ value: 'empty' }, { value: 'empty' }, { value: 'empty' }, { value: 'empty' }, { value: 'empty' }, { value: 'empty' }],
+        [{ value: 'empty' }, { value: 'empty' }, { value: 'empty' }, { value: 'empty' }, { value: 'empty' }, { value: 'empty' }],
+        [{ value: 'empty' }, { value: 'empty' }, { value: 'empty' }, { value: 'empty' }, { value: 'empty' }, { value: 'empty' }],
+        [{ value: 'empty' }, { value: 'empty' }, { value: 'empty' }, { value: 'empty' }, { value: 'empty' }, { value: 'empty' }],
+        [{ value: 'empty' }, { value: 'empty' }, { value: 'empty' }, { value: 'empty' }, { value: 'empty' }, { value: 'empty' }],
+      ],
+    }
+
+    const generatedBoardCells = generateBoardCells(gameState);
+    const firstBorderCell = generatedBoardCells[0];
+
+    expect(firstBorderCell.type).toBe("player1Border");
+  });
+  it("Should returns valid data for last border cell", () => {
+    const gameState: GameState = {
+      turn: 'white',
+      winner: null,
+      winningPath: null,
+      board: [
+        [{ value: 'black' }, { value: 'empty' }, { value: 'empty' }, { value: 'empty' }, { value: 'empty' }, { value: 'empty' }],
+        [{ value: 'empty' }, { value: 'empty' }, { value: 'empty' }, { value: 'empty' }, { value: 'empty' }, { value: 'empty' }],
+        [{ value: 'empty' }, { value: 'empty' }, { value: 'empty' }, { value: 'empty' }, { value: 'empty' }, { value: 'empty' }],
+        [{ value: 'empty' }, { value: 'empty' }, { value: 'empty' }, { value: 'empty' }, { value: 'empty' }, { value: 'empty' }],
+        [{ value: 'empty' }, { value: 'empty' }, { value: 'empty' }, { value: 'empty' }, { value: 'empty' }, { value: 'empty' }],
+        [{ value: 'empty' }, { value: 'empty' }, { value: 'empty' }, { value: 'empty' }, { value: 'empty' }, { value: 'empty' }],
+      ],
+    }
+
+    const generatedBoardCells = generateBoardCells(gameState);
+    const lastBorderCell = generatedBoardCells[61];
+
+    expect(lastBorderCell.type).toBe("player1Border");
+  });
+  it("Should returns valid data for first playable cell", () => {
+    const gameState: GameState = {
+      turn: 'white',
+      winner: null,
+      winningPath: null,
+      board: [
+        [{ value: 'black' }, { value: 'empty' }, { value: 'empty' }, { value: 'empty' }, { value: 'empty' }, { value: 'empty' }],
+        [{ value: 'empty' }, { value: 'empty' }, { value: 'empty' }, { value: 'empty' }, { value: 'empty' }, { value: 'empty' }],
+        [{ value: 'empty' }, { value: 'empty' }, { value: 'empty' }, { value: 'empty' }, { value: 'empty' }, { value: 'empty' }],
+        [{ value: 'empty' }, { value: 'empty' }, { value: 'empty' }, { value: 'empty' }, { value: 'empty' }, { value: 'empty' }],
+        [{ value: 'empty' }, { value: 'empty' }, { value: 'empty' }, { value: 'empty' }, { value: 'empty' }, { value: 'empty' }],
+        [{ value: 'empty' }, { value: 'empty' }, { value: 'empty' }, { value: 'empty' }, { value: 'empty' }, { value: 'empty' }],
+      ],
+    }
+
+    const generatedBoardCell = generateBoardCells(gameState);
+    const firstPlayableCell = generatedBoardCell[8];
+
+    expect(firstPlayableCell.type).toBe("playable");
+    expect(firstPlayableCell.withoutBorderCoordinates).toStrictEqual({ x: 0, y: 0 });
+  });
+  it("Should returns valid data for last playable cell", () => {
+    const gameState: GameState = {
+      turn: 'white',
+      winner: null,
+      winningPath: null,
+      board: [
+        [{ value: 'black' }, { value: 'empty' }, { value: 'empty' }, { value: 'empty' }, { value: 'empty' }, { value: 'empty' }],
+        [{ value: 'empty' }, { value: 'empty' }, { value: 'empty' }, { value: 'empty' }, { value: 'empty' }, { value: 'empty' }],
+        [{ value: 'empty' }, { value: 'empty' }, { value: 'empty' }, { value: 'empty' }, { value: 'empty' }, { value: 'empty' }],
+        [{ value: 'empty' }, { value: 'empty' }, { value: 'empty' }, { value: 'empty' }, { value: 'empty' }, { value: 'empty' }],
+        [{ value: 'empty' }, { value: 'empty' }, { value: 'empty' }, { value: 'empty' }, { value: 'empty' }, { value: 'empty' }],
+        [{ value: 'empty' }, { value: 'empty' }, { value: 'empty' }, { value: 'empty' }, { value: 'empty' }, { value: 'empty' }],
+      ],
+    }
+
+    const generatedBoardCell = generateBoardCells(gameState);
+    const lastPlayableCell = generatedBoardCell[53];
+
+    expect(lastPlayableCell.type).toBe("playable");
+    expect(lastPlayableCell.withoutBorderCoordinates).toStrictEqual({ x: 5, y: 5 });
+  });
 });
