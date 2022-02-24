0e164322935d2ae1b609641ce5a6149193d55aa5
atilbian <arnaud@marmelab.com>
2022-02-24 13:25:19 +0100

    Use auth module

--- web-app/src/app.module.ts
+++ web-app/src/app.module.ts
@@ -3,13 +3,15 @@ import { AppController } from './app.controller';
 import { GamesModule } from './games/games.module';
 import { UsersModule } from './users/users.module';
 import { LocalplayGamesModule } from './api/localplay/games/localplay.games.module';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
   imports: [
     GamesModule,
     UsersModule,
     LocalplayGamesModule,
+    AuthModule
   ],
   controllers: [AppController],
 })
-export class AppModule {}
+export class AppModule { }
