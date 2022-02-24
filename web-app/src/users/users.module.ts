f9637bd7446a7503cfe2bea707e8f4ad963461ea
atilbian <arnaud@marmelab.com>
2022-02-24 13:08:31 +0100

    Update module def

--- web-app/src/users/users.module.ts
+++ web-app/src/users/users.module.ts
@@ -1,5 +1,23 @@
 import { Module } from '@nestjs/common';
+import { UsersService } from './users.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { getConnectionOptions } from 'typeorm';
+import { User } from '../users/user.entity';
+import { ApiUsersController } from './api.users.controller';
 
 @Module({
+  imports: [
+    TypeOrmModule.forRootAsync({
+      useFactory: async () =>
+        Object.assign(await getConnectionOptions(), {
+          autoLoadEntities: true,
+          keepConnectionAlive: true,
+        }),
+    }),
+    TypeOrmModule.forFeature([User]),
+  ],
+  providers: [UsersService],
+  exports: [UsersService],
+  controllers: [ApiUsersController],
 })
 export class UsersModule { }
