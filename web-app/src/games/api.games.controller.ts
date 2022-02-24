3095fcdd6c3300ea7fe0e6c643f5d6c09c468f36
atilbian <arnaud@marmelab.com>
2022-02-24 20:06:55 +0100

    Use new type

--- web-app/src/games/api.games.controller.ts
+++ web-app/src/games/api.games.controller.ts
@@ -8,12 +8,13 @@ import {
   Req,
   UseGuards,
   NotFoundException,
+  Request
 } from '@nestjs/common';
-import { Request } from 'express';
 import { Coordinates } from '../common/utils';
 import { GamesService, GameAndStatus } from './games.service';
 import { Game } from './game.entity';
 import { AuthGuard } from '@nestjs/passport';
+import { UserDataInjectedInRequestAfterAuth } from 'src/auth/auth.service';
 
 @UseGuards(AuthGuard('jwt'))
 @Controller('api/games')
@@ -23,39 +24,40 @@ export class ApiGamesController {
   @Post('')
   async create(
     @Body() params: { size?: number },
-    @Req() req: Request
+    @Request() req: { user: UserDataInjectedInRequestAfterAuth }
   ): Promise<Game> {
-    return await this.gamesService.createNewGame(params.size, req.user["username"]);
+    return await this.gamesService.createNewGame(params.size, req.user.username);
   }
 
   @Get(':id')
   async findOne(
     @Param('id') id: number,
-    @Req() req: Request
+    @Request() req: { user: UserDataInjectedInRequestAfterAuth }
   ): Promise<GameAndStatus> {
     const game = await this.gamesService.findGameById(id);
     if (game === undefined) throw new NotFoundException();
-    return this.gamesService.getGameAndStatus(game, req.user["username"]);
+    return this.gamesService.getGameAndStatus(game, req.user.username);
   }
 
   @Put(':id')
   async update(
     @Param('id') id: number,
     @Body() params: { nextMove: Coordinates },
-    @Req() req: Request
+    @Request() req: { user: UserDataInjectedInRequestAfterAuth }
   ): Promise<GameAndStatus> {
     let game = await this.gamesService.findGameById(id);
     if (game === undefined) throw new NotFoundException();
     game = await this.gamesService.updateGameState(game, params.nextMove);
-    return this.gamesService.getGameAndStatus(game, req.user["username"]);
+    return this.gamesService.getGameAndStatus(game, req.user.username);
   }
 
   @Get(':id/join')
   async joinGame(
     @Param('id') id: number,
-    @Req() req: Request) {
+    @Request() req: { user: UserDataInjectedInRequestAfterAuth }
+  ): Promise<Game> {
     const game = await this.gamesService.findGameById(id);
     if (game === undefined) throw new NotFoundException();
-    await this.gamesService.joinGame(id, req.user["username"]);
+    return await this.gamesService.joinGame(id, req.user.username);
   }
 }
