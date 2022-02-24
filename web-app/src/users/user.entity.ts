29cfe2f0f1df01b6234d70e73f9e09bc9c6474f3
atilbian <arnaud@marmelab.com>
2022-02-24 13:07:50 +0100

    Update user entity

--- web-app/src/users/user.entity.ts
+++ web-app/src/users/user.entity.ts
@@ -5,9 +5,12 @@ export class User {
   @PrimaryGeneratedColumn()
   id: number;
 
-  @Column()
+  @Column({ unique: true })
   username: string;
 
   @Column()
+  password: string;
+
+  @Column({ nullable: true })
   lastSessionId: string;
 }
