6896ef5e736ed4858efff98ed5c75a8daf17b21c
atilbian <arnaud@marmelab.com>
2022-02-25 12:31:25 +0100

    Display a message + join code when waiting for a player

--- mobile-app/src/screens/RemoteScreen.tsx
+++ mobile-app/src/screens/RemoteScreen.tsx
@@ -20,17 +20,15 @@ export function RemoteScreen({ navigation, route }: RemoteScreenProps) {
     updateGame(gameState.game, coordinates).then(setGameState);
   }
 
-  return (
-
-    gameState && (
-      <View style={{ flex: 1, flexDirection: 'column', alignItems: 'center', justifyContent: 'center' }}>
-        <Text>Ready to play: {String(gameState.readyToPlay)}</Text>
-        <Text>Current player turn: {String(gameState.currentPlayerTurnToPlay)}</Text>
-        <ScrollView horizontal>
-          <Board gameState={gameState.game.state} onCellPress={onCellPress} />
-        </ScrollView>
-      </View>
-    )
-
-  );
+  return gameState ?
+    gameState.readyToPlay ? <View style={{ flex: 1, flexDirection: 'column', alignItems: 'center', justifyContent: 'center' }}>
+      <ScrollView horizontal>
+        <Board gameState={gameState.game.state} onCellPress={onCellPress} />
+      </ScrollView>
+    </View>
+      : <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+        <Text style={{ fontSize: 24 }}> Waiting for an opponent..</Text>
+        <Text style={{ fontSize: 18 }}> Use this join code : {route.params.gameId}</Text>
+      </View >
+    : <View />
 }
