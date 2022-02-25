f9b5581af41947e76c7af0ed1d325da1c5da0bad
atilbian <arnaud@marmelab.com>
2022-02-25 09:35:25 +0100

    Keep old api route

--- web-app/src/localgames/localgames.controller.ts
+++ web-app/src/localgames/localgames.controller.ts
@@ -2,7 +2,7 @@ import { Body, Controller, Post, Put } from '@nestjs/common';
 import { GameState, initNewGameState, updateGameState } from '../common/gameState';
 import { Coordinates } from '../common/utils';
 
-@Controller('api/localgames/games')
+@Controller('api/localplay/games')
 export class LocalGamesModuleController {
 
     @Post('')
