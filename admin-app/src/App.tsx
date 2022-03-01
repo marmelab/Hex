8eb732fda82ab241c3ff3e0adcb125a46cd5a017
atilbian <arnaud@marmelab.com>
2022-03-01 17:40:20 +0100

    Use it

--- admin-app/src/App.tsx
+++ admin-app/src/App.tsx
@@ -1,11 +1,12 @@
 import * as React from 'react';
-import { Admin, Resource, ListGuesser } from 'react-admin';
+import { Admin, Resource } from 'react-admin';
 import jsonServerProvider from 'ra-data-json-server';
+import { GameList } from './components/GameList';
 
 const dataProvider = jsonServerProvider('https://jsonplaceholder.typicode.com');
 const App = () => (
   <Admin dataProvider={dataProvider}>
-    <Resource name="users" list={ListGuesser} />
+    <Resource name="games" list={GameList} />
   </Admin>
 );
 
