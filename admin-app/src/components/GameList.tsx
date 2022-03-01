6c3af3b55634ca03413f31128f00ab2d3b024f68
atilbian <arnaud@marmelab.com>
2022-03-01 17:40:13 +0100

    Add game list compo

---
+++ admin-app/src/components/GameList.tsx
@@ -0,0 +1,16 @@
+import * as React from 'react';
+import { List, Datagrid, TextField, DateField, NumberField } from 'react-admin';
+
+interface GameListProps {}
+
+export const GameList = (props: GameListProps) => (
+  <List {...props}>
+    <Datagrid rowClick="edit">
+      <TextField source="id" />
+      <DateField source="createdAt" />
+      <DateField source="updatedAt" />
+      <NumberField source="player1.id" />
+      <NumberField source="player2.id" />
+    </Datagrid>
+  </List>
+);
