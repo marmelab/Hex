2e42d3debfb9fe4e1477ab40b228938c5a15d032
atilbian <arnaud@marmelab.com>
2022-03-03 14:54:05 +0100

    Add a first test example

--- admin-app/src/components/GameList.test.tsx
+++ admin-app/src/components/GameList.test.tsx
@@ -1,8 +1,15 @@
 import * as React from 'react';
-import { render } from '@testing-library/react';
+import { render, screen } from '@testing-library/react';
 import { GameList } from './GameList';
+import { Admin, Resource } from 'react-admin';
+import fakeDataProvider from 'ra-data-fakerest';
+import gamesTestData from '../fixtures/games.json';
 
-test('Render a game list without any error', () => {
-  render(<GameList></GameList>);
-  expect(() => render(<GameList></GameList>)).not.toThrowError();
+test('Render a game list without any error', async () => {
+  render(
+    <Admin dataProvider={fakeDataProvider(gamesTestData)}>
+      <Resource name="games" list={GameList} />
+    </Admin>,
+  );
+  expect(await screen.findAllByText(/Add filter/)).toHaveLength(1);
 });
