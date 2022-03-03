89d2587324100dd34d61f8864169493423151c6c
atilbian <arnaud@marmelab.com>
2022-03-03 12:30:00 +0100

    Clean getList func

--- admin-app/src/data-provider.ts
+++ admin-app/src/data-provider.ts
@@ -1,4 +1,4 @@
-import { fetchUtils, Options } from 'react-admin';
+import { fetchUtils, Options, DataProvider } from 'react-admin';
 import { stringify } from 'query-string';
 
 const adminApiUrl = `${import.meta.env.VITE_HEX_ADMIN_API_URL}/admin`;
@@ -17,22 +17,20 @@ const httpClient = (url: string, options: Options = {}) => {
   return fetchUtils.fetchJson(url, options);
 };
 
-export default {
-  getList: (resource: string, params: any) => {
-    console.log(params);
+const dataProvider: DataProvider = {
+  getList: (resource, params) => {
+    const { page, perPage } = params.pagination;
+    const { field, order } = params.sort;
     const query = {
-      s: {
-        skip: (params.pagination.page - 1) * params.pagination.perPage,
-        take: params.pagination.perPage,
-        sort: [{ column: params.sort.field, order: params.sort.order }],
-        filter: Object.keys(params.filter).map((key) => ({
-          column: key,
-          value: params.filter[key],
-        })),
-      },
+      skip: (page - 1) * perPage,
+      take: perPage,
+      sort: [{ column: field, order: order }],
+      filter: Object.keys(params.filter).map((key) => ({
+        column: key,
+        value: params.filter[key],
+      })),
     };
-    console.log(JSON.stringify(query));
-    const url = `${adminApiUrl}/${resource}?${JSON.stringify(query)}`;
+    const url = `${adminApiUrl}/${resource}?s=${JSON.stringify(query)}`;
 
     return httpClient(url).then(({ json }) => ({
       data: json.data,
@@ -40,51 +38,35 @@ export default {
     }));
   },
 
-  // getList: (resource, params) => {
-  //   const { page, perPage } = params.pagination;
-  //   const { field, order } = params.sort;
-  //   const query = {
-  //     sort: JSON.stringify([field, order]),
-  //     range: JSON.stringify([(page - 1) * perPage, page * perPage - 1]),
-  //     filter: JSON.stringify(params.filter),
-  //   };
-  //   const url = `${apiUrl}/${resource}?${stringify(query)}`;
-
-  //   return httpClient(url).then(({ headers, json }) => ({
-  //     data: json,
-  //     total: parseInt(headers.get('content-range').split('/').pop(), 10),
-  //   }));
-  // },
-
-  getOne: (resource: any) => {
+  getOne: (resource) => {
     throw Error('This function is not yet implemented');
   },
 
-  getMany: (resource: any) => {
+  getMany: (resource) => {
     throw Error('This function is not yet implemented');
   },
 
-  getManyReference: (resource: any) => {
+  getManyReference: (resource) => {
     throw Error('This function is not yet implemented');
   },
 
-  update: (resource: any) => {
+  update: (resource) => {
     throw Error('This function is not yet implemented');
   },
 
-  updateMany: (resource: any) => {
+  updateMany: (resource) => {
     throw Error('This function is not yet implemented');
   },
 
-  create: (resource: any) => {
+  create: (resource) => {
     throw Error('This function is not yet implemented');
   },
 
-  delete: (resource: any) => {
+  delete: (resource) => {
     throw Error('This function is not yet implemented');
   },
 
-  deleteMany: (resource: string, params: { ids: number[] }) => {
+  deleteMany: (resource, params) => {
     const query = {
       ids: JSON.stringify(params.ids),
     };
@@ -93,3 +75,5 @@ export default {
     }).then(({ json }) => ({ data: json }));
   },
 };
+
+export default dataProvider;
