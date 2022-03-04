d89fcbef67fd2e13e507423014b09a56f3f12872
atilbian <arnaud@marmelab.com>
2022-03-04 11:33:48 +0100

    Add subscriber to increment nbGames when required

---
+++ web-app/src/games/games.subscriber.ts
@@ -0,0 +1,33 @@
+import {
+  Connection,
+  EntitySubscriberInterface,
+  EventSubscriber,
+  InsertEvent,
+  UpdateEvent,
+} from 'typeorm';
+import { UsersService } from 'src/users/users.service';
+import { Game } from './game.entity';
+
+@EventSubscriber()
+export class GamesSubscriber implements EntitySubscriberInterface<Game> {
+  constructor(
+    connection: Connection,
+    private readonly userService: UsersService,
+  ) {
+    connection.subscribers.push(this);
+  }
+
+  listenTo() {
+    return Game;
+  }
+
+  afterInsert(event: InsertEvent<Game>): void | Promise<any> {
+    this.userService.incrementNbGames(event.entity.player1.id);
+  }
+
+  afterUpdate(event: UpdateEvent<Game>): void | Promise<any> {
+    if (event.updatedRelations.some((x) => x.propertyName == 'player2')) {
+      this.userService.incrementNbGames(event.entity.player2.id);
+    }
+  }
+}
