e9ef2597ea6fd909a0b8f4032ed30e9339efa1f4
atilbian <arnaud@marmelab.com>
2022-03-09 12:57:25 +0100

    Extract code into multiple funcs and rename stuffs

--- web-app/src/common/prediction.ts
+++ web-app/src/common/prediction.ts
@@ -2,26 +2,46 @@ import { GameState, StoneColor } from './gameState';
 import { Coordinates, deepCloneObject } from './utils';
 import { getNbMovesNeededToWin } from './pathfinding';
 
+interface PlayPrediction {
+  coordinates: Coordinates;
+  playerRemainingMovesToWin: number;
+  opponentRemainingMovesToWin: number;
+  score: number;
+}
+
 export function getNextPlaySuggestion(gameState: GameState, stoneColor: StoneColor): Coordinates {
-  const allPotentialPlays = [];
+  const playableCells = getPlayableCells(gameState);
+  if (playableCells.length === 0) throw new Error("There is no playable cell in the given board.")
+  const playPredictions = playableCells.map(coordinates =>
+    getPlayPrediction(gameState, coordinates, stoneColor)
+  );
+  return getBestPossiblePlay(playPredictions).coordinates;
+}
+
+function getPlayableCells(gameState: GameState): Coordinates[] {
+  const playableCells = [];
   gameState.board.forEach((row, y) => {
     row.forEach((cell, x) => {
       if (cell.value === "empty")
-        allPotentialPlays.push({ x, y })
+        playableCells.push({ x, y })
     })
   });
+  return playableCells;
+}
 
-  if (allPotentialPlays.length === 0) throw new Error("There is no playable cell in the given board.")
+function getPlayPrediction(gameState: GameState, coordinates: Coordinates, stoneColor: StoneColor): PlayPrediction {
+  const potentialGameState = deepCloneObject(gameState) as GameState;
+  potentialGameState.board[coordinates.y][coordinates.x].value = stoneColor;
+  const playerRemainingMovesToWin = getNbMovesNeededToWin(potentialGameState, stoneColor);
+  const opponentColor = stoneColor === "black" ? "white" : "black";
+  const opponentRemainingMovesToWin = getNbMovesNeededToWin(potentialGameState, opponentColor);
+  return { coordinates, playerRemainingMovesToWin, opponentRemainingMovesToWin, score: playerRemainingMovesToWin - opponentRemainingMovesToWin }
+}
 
-  const winCostForPotentialPlays = allPotentialPlays.map(playPosition => {
-    const potentialGameState = deepCloneObject(gameState) as GameState;
-    potentialGameState.board[playPosition.y][playPosition.x].value = stoneColor;
-    const opponentColor = stoneColor === "black" ? "white" : "black";
-    return { playPosition, myWinCost: getNbMovesNeededToWin(potentialGameState, stoneColor), myAdvWinCost: getNbMovesNeededToWin(potentialGameState, opponentColor), winCost: getNbMovesNeededToWin(potentialGameState, stoneColor) - getNbMovesNeededToWin(potentialGameState, opponentColor) }
+function getBestPossiblePlay(potentialPlays: PlayPrediction[]): PlayPrediction {
+  const winningPlays = potentialPlays.filter(play => play.playerRemainingMovesToWin === 0);
+  if (winningPlays.length > 0) return winningPlays[0];
+  return potentialPlays.reduce(function (prev, curr) {
+    return prev.score <= curr.score ? prev : curr;
   });
-
-  const winningPlays = winCostForPotentialPlays.filter(play => play.myWinCost === 0);
-  return winningPlays.length > 0 ? winningPlays[0].playPosition : winCostForPotentialPlays.reduce(function (prev, curr) {
-    return prev.winCost <= curr.winCost ? prev : curr;
-  }).playPosition;
 }
