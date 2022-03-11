1b778ac87bed6e5b9593a4e88a2fd8c838c71c8b
Jean-Baptiste Kaiser <jb@marmelab.com>
2022-03-11 13:04:29 +0100

    add 3x3 and 5x5 test cases

--- web-app/src/common/prediction.spec.ts
+++ web-app/src/common/prediction.spec.ts
@@ -281,4 +281,124 @@ describe('Get a suggestion for the next play based on minimax', () => {
       ]),
     ).toBeTruthy();
   });
+
+  it('Should get a suggestion to play at (0, 1) as WHITE on this 3x3 board since black is about to win', () => {
+    const input = parseGameStateFromMultilineString(`
+  ⬡ ⬡ W
+   ⬡ ⬢ ⬢
+    W ⬡ ⬡
+  `);
+
+    const nextPlayAdvice = getMinimaxNextPlaySuggestion(
+      input.board,
+      'white',
+      2,
+    );
+
+    expect(
+      areExpectedCoordinatesInList(nextPlayAdvice, [{ x: 0, y: 1 }]),
+    ).toBeTruthy();
+  });
+
+  it('Should get a suggestion to play at (0, 1) or (2, 1) as BLACK on this 3x3 board', () => {
+    const input = parseGameStateFromMultilineString(`
+  ⬡ ⬡ W
+   ⬡ ⬢ ⬡
+    W ⬡ ⬡
+  `);
+
+    const nextPlayAdvice = getMinimaxNextPlaySuggestion(
+      input.board,
+      'black',
+      2,
+    );
+
+    expect(
+      areExpectedCoordinatesInList(nextPlayAdvice, [
+        { x: 0, y: 1 },
+        { x: 2, y: 1 },
+      ]),
+    ).toBeTruthy();
+  });
+
+  it('Should get a suggestion to play at (0, 1) as WHITE on this 5x5 board with depth 2 minimax', () => {
+    const input = parseGameStateFromMultilineString(`
+  ⬡ ⬡ ⬡ W ⬡
+   ⬡ ⬢ W W ⬢
+    W ⬢ W ⬢ ⬢
+     W ⬢ ⬢ W ⬢
+      W ⬢ W ⬢ W
+  `);
+
+    const nextPlayAdvice = getMinimaxNextPlaySuggestion(
+      input.board,
+      'white',
+      2,
+    );
+
+    expect(
+      areExpectedCoordinatesInList(nextPlayAdvice, [{ x: 0, y: 1 }]),
+    ).toBeTruthy();
+  });
+
+  it('Should get a suggestion to play at (1, 1) as BLACK on this 5x5 board with depth 2 minimax', () => {
+    const input = parseGameStateFromMultilineString(`
+  ⬡ ⬡ ⬡ W ⬡
+   ⬡ ⬡ W W ⬢
+    W ⬢ W ⬢ ⬢
+     W ⬢ ⬢ W ⬢
+      W ⬢ W ⬢ W
+  `);
+
+    const nextPlayAdvice = getMinimaxNextPlaySuggestion(
+      input.board,
+      'black',
+      2,
+    );
+
+    expect(
+      areExpectedCoordinatesInList(nextPlayAdvice, [{ x: 1, y: 1 }]),
+    ).toBeTruthy();
+  });
+
+  it('Should get a suggestion to play at (0, 2) as WHITE on this 5x5 board with depth 2 minimax', () => {
+    const input = parseGameStateFromMultilineString(`
+  ⬡ ⬡ ⬡ W ⬡
+   ⬡ ⬡ W W ⬢
+    ⬡ ⬢ W ⬢ ⬢
+     W ⬢ ⬢ W ⬢
+      W ⬢ W ⬢ W
+  `);
+
+    const nextPlayAdvice = getMinimaxNextPlaySuggestion(
+      input.board,
+      'white',
+      2,
+    );
+
+    expect(
+      areExpectedCoordinatesInList(nextPlayAdvice, [{ x: 0, y: 2 }]),
+    ).toBeTruthy();
+  });
+
+  it('Should get a suggestion to play at (0, 2) as WHITE on this 5x5 board with depth 3 minimax', () => {
+    const input = parseGameStateFromMultilineString(`
+  ⬡ ⬡ ⬡ W ⬡
+   ⬡ ⬡ W W ⬢
+    ⬡ ⬢ W ⬢ ⬢
+     W ⬢ ⬢ W ⬢
+      W ⬢ W ⬢ W
+  `);
+
+    const nextPlayAdvice = getMinimaxNextPlaySuggestion(
+      input.board,
+      'white',
+      3,
+    );
+
+    expect(
+      areExpectedCoordinatesInList(nextPlayAdvice, [{ x: 0, y: 2 }]),
+    ).toBeTruthy();
+  });
 });
+
